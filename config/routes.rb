01b0f786c0077efaae490ecb49b65a653413bdca
unknown <jonaspng2000@gmail.com>
2021-06-14 23:28:56 +0800

    third version

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'register/error', to: 'users#create_error'
   get 'register/success', to: 'users#create_success'
   get 'api/auth', to: 'sessions#persist'
-  get 'api/login', to: 'sessions#validation'
+  get 'api/error', to: 'sessions#validation'
   post 'api/login', to: 'sessions#create'
   post 'api/logout', to: 'sessions#destroy'
   get 'api/chart', to: 'sessions#chart'
