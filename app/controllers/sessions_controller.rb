cf74325ce3b959968e4d9569850464937f916be8
Jonas <jonaspng2000@gmail.com>
2021-12-19 21:30:45 +0800

    testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
   before_action :logged_in?
   before_action :require_user, except: [:create, :validation, :persist]
 
+
   def create
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params.require(:password))
       session[:user_id] = user.id
       redirect_to 'https://todolist-cvwo.herokuapp.com/dashboard'
+      $is_logged_in = true
     else
       redirect_to 'https://todolist-cvwo.herokuapp.com/login'
       $error = true
@@ -15,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def persist
-    if logged_in?
+    if logged_in? || $is_logged_in
       render json: {"auth": true}
     else
       render json: {"auth": false}
