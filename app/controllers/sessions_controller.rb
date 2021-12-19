fe6c6cf420ddb96d96b08c305dcee98ade4764a7
Jonas <jonaspng2000@gmail.com>
2021-12-19 21:24:59 +0800

    testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  
-  before_action :require_user, except: [:create, :validation, :persist]
+
 
   def create
     user = User.find_by(email: params[:email].downcase)
