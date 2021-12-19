34798b83b4ba263a1d5122ab1cf2ac06adeaed38
Jonas <jonaspng2000@gmail.com>
2021-12-19 17:29:01 +0800

    debugging error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, raise: false
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
