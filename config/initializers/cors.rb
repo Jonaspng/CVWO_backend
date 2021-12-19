47f1c07760c4269585f4e90b63b4fb8924b2c569
Jonas <jonaspng2000@gmail.com>
2021-12-19 20:29:49 +0800

    testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://todolist-cvwo.herokuapp.com'
-    resource '*', 
-    headers: :any, 
-    methods: [:get, :post, :patch, :put, :delete], 
+    origins 'https://todolist-cvwo.herokuapp.com/'
+    resource '*',
+    headers: :any,
+    methods: [:get, :post, :patch, :put, :delete],
     credentials: true
   end
 end
