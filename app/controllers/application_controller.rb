9dcb5bb411375d406567791bf1d63fc40cf17d52
Jonas <jonaspng2000@gmail.com>
2021-12-19 20:51:02 +0800

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::API
+  skip_before_action :verify_authenticity_token, raise: false
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
