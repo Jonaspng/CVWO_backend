453321db733eda37ac75f247f9e0ffd2af17f322
Jonaspng <jonaspng@hotmail.com>
2022-01-02 11:31:01 +0800

    editing cors policy settings

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module CVWOBackend
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://todolist-cvwo.herokuapp.com'
+        origins 'https://todolist-cvwo.herokuapp.com/'
         resource '*',
         headers: :any,
         methods: [:get, :patch, :put, :delete, :post, :options]
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://todolist-cvwo.herokuapp.com'
+    origins 'https://todolist-cvwo.herokuapp.com/'
     resource '*',
     headers: :any,
     methods: [:get, :post, :patch, :put, :delete],
