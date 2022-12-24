91ece1e38fabbb6e6ae57d6a6bf1f1f67255c9d2
Jonaspng <jonaspng@hotmail.com>
2022-12-24 14:56:46 +0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://todolist-cvwo.herokuapp.com'
+    origins 'https://todolist-cvwo.up.railway.app'
     resource '*',
     headers: :any,
     methods: [:get, :post, :patch, :put, :delete],
