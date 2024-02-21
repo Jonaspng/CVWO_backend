70a29b258532d8c4c969415529fda344e562e943
Jonaspng <jonaspng@hotmail.com>
2024-02-21 21:34:52 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ... other webpack configuration options
+  output: {
+    // ... other output options
+
+    // Set the hash function to 'sha512'
+    hashFunction: 'sha512',
+  },
+};
